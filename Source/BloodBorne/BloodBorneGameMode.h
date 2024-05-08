5b4ae7d9ebcbcf10c0e0d9dbea80758a682a9c26
nixeast <eastnix@gmail.com>
2024-05-08 23:07:27 +0900

    Initial commit

---
+++ Source/BloodBorne/BloodBorneGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BloodBorneGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ABloodBorneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ABloodBorneGameMode();
+};
+
+
+
